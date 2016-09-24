5ceb8b08b4357104ba45df06645ba5d91ce045b8
Ji Baik <jiwoongb@gmail.com>
2016-09-24 14:59:05 -0700

    (chore) Create dummy data

---
+++ data.js
@@ -0,0 +1,63 @@
+// var orgs = {
+//   name:{
+//     name: 'name',
+//     location: [1,1],
+//     description: 'description',
+//     category: ['stringz'],
+//     serviceArea: [[1,1],[1,1]],
+//     events:[{
+//       name:'name',
+//       location: [1,1],
+//       description: 'description',
+//       date: 'string',
+//       time: 'string'
+//     }]
+//   }
+// }
+
+
+// DUMMY ORG DATA
+var orgs = {
+  'Helping Hand':{
+   name: 'Helping Hand',
+    location: [37.784784, -122.410136],
+    description: 'description',
+    category: ['stringz'],
+    serviceArea: [[1,1],[1,1]],
+    events:[{
+      name:'First Event',
+      location: [37.784784, -122.410136],
+      description: 'description',
+      date: '09/25/16',
+      time: ['16:30','22:00'}
+    }]
+  },
+  'Urban Helpers':{
+    name: 'Urban Helpers',
+    location: [37.753726, -122.387846],
+    description: 'description',
+    category: ['stringz'],
+    serviceArea: [[1,1],[1,1]],
+    events:[{
+      name:'First Event',
+      location: [37.753726, -122.387846],
+      description: 'description',
+      date: '09/25/16',
+      time: ['17:45','20:00']
+    }]
+  },
+   'Mountain Helpers':{
+    name: 'Mountain Helpers',
+    location: [37.786962, -122.458764],
+    description: 'description',
+    category: ['stringz'],
+    serviceArea: [[1,1],[1,1]],
+    events:[{
+      name:'First Event',
+      location: [37.786962, -122.458764],
+      description: 'description',
+      date: '09/25/16',
+      time: ['12:00','17:30']
+    }]
+  }
+}
