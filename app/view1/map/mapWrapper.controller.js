f620f70cbf445ffa464ec90644aae459c32c7f25
Brendan Dugan <brendan@workspan.com>
2016-09-24 20:53:52 -0700

    add polygons

--- app/view1/map/mapWrapper.controller.js
+++ app/view1/map/mapWrapper.controller.js
@@ -17,12 +17,6 @@ angular.module('myApp.mapWrapper')
       mc.map.showInfoWindow(event, $index);
       mc.setOrg({'orgName': o.name});
     };
-    var i = 0;
-    mc.getOrg = function($index) {
-      console.log('$index here: ', $index);
-      console.log('org here: ', mc.orgs[$index]);
-      return mc.orgs[$index]
-    }
 
     mc.showInfoWindow = function(event, $index,o) {
       var infowindow = new google.maps.InfoWindow();
@@ -37,10 +31,28 @@ angular.module('myApp.mapWrapper')
 
       infowindow.open(mc.map);
       mc.setOrg({'orgName': o.name});
-    }
+    };
+
+    mc.mapClickHandler = function(event) {
+      console.log('event: ', event);
+      mc.path.push([event.latLng.lat(), event.latLng.lng()]);
+      console.log('path:', mc.path);
+    };
+    
+    mc.getColor = function(category) {
+      var _colorMap = {
+        'healthcare': '#FF0000',
+        'food': '#00FF00',
+        'exercise': '#0000FF'
+      };
+      console.log('category: ', category);
+      console.log('color: ', _colorMap[category]);
+      return _colorMap[category];
+    };
 
     mc.$onInit = function() {
       console.log('setOrgCb: ', mc.setOrg);
+      mc.path = [];
       mc.orgs = Object.keys(DataFactory.orgs).map(function(name){
         return DataFactory.orgs[name];
       });
