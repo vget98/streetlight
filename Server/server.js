b3976eca22f55900050c5a44c2cfe46b5251f14a
Patrick Truong <pahtruon@gmail.com>
2016-10-30 16:07:52 -0700

    (bug) fix bug loading up wrong html file

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var http = requie("http");
+var http = require("http");
 
 var app = express();
 var server = http.createServer(app);
 
 // Set up server middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '/../app')));
 
 // Run Server
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
+
 server.listen(port, function() {
   console.log("Listening on port", port);
 });
 
-// Server up static index.html
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, "..app/index.html"));
-});
+module.exports = app;
